746bbf981b83b981acec1a473324b8971f43db69
IzabellaBarbosa <izabella.psbarbosa@gmail.com>
2020-11-18 17:31:19 -0300

    Adicionando pasta frontend

---
+++ Frontend/carousel.js
@@ -0,0 +1,10 @@
+$('.carousel').slick({
+    dots:true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: $('#arrow-prev'),
+    nextArrow: $('#arrow-next'),
+    centerPadding: '50px'
+})
