c9f71873c19d374fdfe0819dc7fbe7f3476f8a47
IzabellaBarbosa <izabella.psbarbosa@gmail.com>
2020-11-17 22:41:02 -0300

    Validações de funcionários

--- Backend/controller/FuncionariosController.js
+++ Backend/controller/FuncionariosController.js
@@ -1,4 +1,5 @@
 const FuncionarioDao = require('../dao/FuncionarioDao');
+const { validationResult } = require('express-validator')
 
 const funcionarioDao = new FuncionarioDao();
 
@@ -7,14 +8,20 @@ class FuncionarioController{
     cadastro(req, res){
         const funcionario = req.body;
 
-        funcionarioDao.inserir(funcionario, (erro)=>{
-            if(erro){
-                console.log(erro);
-            }else{
-                console.log("Funcionário cadastrado!");
-                res.status(201).send("Funcionário cadastrado com sucesso");
-            }
-        })
+        const erros = validationResult(req);
+
+        if(!erros.isEmpty()){
+            console.log("Ocorreram erros na validação " + erros);
+        }else{
+            funcionarioDao.inserir(funcionario, (erro)=>{
+                if(erro){
+                    console.log(erro);
+                }else{
+                    console.log("Funcionário cadastrado!");
+                    res.status(201).send("Funcionário cadastrado com sucesso");
+                }
+            });
+        }
     }
 }
 
--- Backend/rotas/RotasFuncionario.js
+++ Backend/rotas/RotasFuncionario.js
@@ -1,10 +1,11 @@
 const router = Router();
 const FuncionarioController = require('../controller/FuncionariosController');
+const Funcionario = require('../model/Funcionario')
 
 const funcionarioController = new FuncionarioController();
 
 //Qual é o mecanismo de busca do funcionário?
     router.route('/funcionarios/cadastro')
-        .post(funcionarioController.cadastro);
+        .post(Funcionario.validacoes(),funcionarioController.cadastro);
     router.route()
         .get();
