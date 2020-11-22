0071f6feb0751b0de460a5e8d9d51252d3ea3eb8
IzabellaBarbosa <izabella.psbarbosa@gmail.com>
2020-11-21 21:43:42 -0300

    Adicionando usuário e verificando validações

--- Backend/controller/OrganizacaoController.js
+++ Backend/controller/OrganizacaoController.js
@@ -1,21 +1,40 @@
 const OrganizacaoDao = require('../dao/OrganizacaoDao');
+const UsuarioController = require("./UsuarioController");
+const { validationResult } = require ('express-validator');
 
 const organizacaoDao = new OrganizacaoDao();
+const usuarioController = new UsuarioController();
 
 class OrganizacaoController{
     
     cadastro(req, res){
         let organizacao = req.body;
 
-        organizacaoDao.inserir(organizacao, (erro) =>{
-            if(erro){
-                console.log(erro);
-                res.send("Ocorreu um erro");
-            }else{
-                console.log("Organização cadastrada!");
-                res.status(201).send("Organização cadastrada com sucesso!");
-            }
-        });
+        let erros = validationResult(req);
+        
+        if(!erros.isEmpty()){
+            console.log(erros);
+            res.send("Erros de validação " + erros);
+        }else{
+            //inserindo organização na tabela de usuário
+            usuarioController.inserirUsuario(
+                organizacao.email,
+                organizacao.senha,
+                "NÃO"
+            );
+            
+            organizacaoDao.inserir(organizacao, (erro) =>{
+                if(erro){
+                    console.log(erro);
+                    res.send("Ocorreu um erro");
+                }else{
+                    console.log("Organização cadastrada!");
+                    res.status(201).send("Organização cadastrada com sucesso!");
+                }
+            });
+        }
+
+        
     }
 }
 
