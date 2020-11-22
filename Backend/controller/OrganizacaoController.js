6879f6800575725cfbc6a2b3d09c2acac9c9c594
IzabellaBarbosa <izabella.psbarbosa@gmail.com>
2020-11-21 22:34:29 -0300

    Aplicando o prettier no controller

--- Backend/controller/OrganizacaoController.js
+++ Backend/controller/OrganizacaoController.js
@@ -1,41 +1,38 @@
-const OrganizacaoDao = require('../dao/OrganizacaoDao');
+const OrganizacaoDao = require("../dao/OrganizacaoDao");
 const UsuarioController = require("./UsuarioController");
-const { validationResult } = require ('express-validator');
+const { validationResult } = require("express-validator");
 
 const organizacaoDao = new OrganizacaoDao();
 const usuarioController = new UsuarioController();
 
-class OrganizacaoController{
-    
-    cadastro(req, res){
-        const organizacao = req.body;
+class OrganizacaoController {
+  cadastro(req, res) {
+    const organizacao = req.body;
 
-        let erros = validationResult(req);
-        //verificando se houve erro de validação
-        if(!erros.isEmpty()){
-            console.log(erros);
-            res.send("Erros de validação " + erros);
-        }else{
-            //inserindo organização na tabela de usuário
-            usuarioController.inserirUsuario(
-                organizacao.email,
-                organizacao.senha,
-                "NÃO"
-            );
-            //inserindo o organização na tabela de organizações
-            organizacaoDao.inserir(organizacao, (erro) =>{
-                if(erro){
-                    console.log(erro);
-                    res.send("Ocorreu um erro");
-                }else{
-                    console.log("Organização cadastrada!");
-                    res.status(201).send("Cadastrado com sucesso!");
-                }
-            });
+    let erros = validationResult(req);
+    //verificando se houve erro de validação
+    if (!erros.isEmpty()) {
+      console.log(erros);
+      res.send("Erros de validação " + erros);
+    } else {
+      //inserindo organização na tabela de usuário
+      usuarioController.inserirUsuario(
+        organizacao.email,
+        organizacao.senha,
+        "NÃO"
+      );
+      //inserindo o organização na tabela de organizações
+      organizacaoDao.inserir(organizacao, (erro) => {
+        if (erro) {
+          console.log(erro);
+          res.send("Ocorreu um erro");
+        } else {
+          console.log("Organização cadastrada!");
+          res.status(201).send("Cadastrado com sucesso!");
         }
-
-        
+      });
     }
+  }
 }
 
 module.exports = OrganizacaoController;
