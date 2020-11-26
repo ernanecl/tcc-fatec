438af35f2417f11bcbc9a2a2c3ce5434e653d0ed
IzabellaBarbosa <izabella.psbarbosa@gmail.com>
2020-11-25 21:54:17 -0300

    Alteração de organização

--- Backend/controller/OrganizacaoController.js
+++ Backend/controller/OrganizacaoController.js
@@ -1,6 +1,8 @@
 const OrganizacaoDao = require("../dao/OrganizacaoDao");
 const UsuarioController = require("./UsuarioController");
 const { validationResult } = require("express-validator");
+const alterarRgCpf = require("../utils/alterarRgCpf");
+const verificandoAlteracao = require("../utils/verificandoAlteracao");
 
 const organizacaoDao = new OrganizacaoDao();
 const usuarioController = new UsuarioController();
@@ -30,6 +32,23 @@ class OrganizacaoController {
           console.log("Organização cadastrada!");
           res.status(201).send("Cadastrado com sucesso!");
         }
+      });
+    }
+  }
+  alterar(req, res){
+    const email = req.params.email;
+    const valores = req.body;
+
+    if(alterarRgCpf(valores, res)){
+      return;
+    }else{
+      organizacaoDao.atualizar(email, valores, (erro, resultado) =>{
+        if(erro){
+          console.log("Ocorreu um erro " + erro);
+          res.status(500).send("Ocorreu um erro");
+          return;
+        }
+        verificandoAlteracao(resultado, res);
       });
     }
   }
