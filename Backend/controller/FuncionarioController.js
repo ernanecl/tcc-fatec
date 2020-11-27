b8c550995ee1b8594aeeabd11dff830dcf10ad42
IzabellaBarbosa <izabella.psbarbosa@gmail.com>
2020-11-26 21:51:55 -0300

    Consulta de funcionário

--- Backend/controller/FuncionarioController.js
+++ Backend/controller/FuncionarioController.js
@@ -2,12 +2,61 @@ const FuncionarioDao = require("../dao/FuncionarioDao");
 const { validationResult } = require("express-validator");
 const UsuarioController = require("./UsuarioController");
 const alterarRgCpf = require("../utils/alterarRgCpf");
-const verificarAlteracao = require("../utils/verificandoAlteracao");
-
+const verificarAlteracao = require("../utils/verificarAlteracao");
+const verificarResultado= require("../utils/verificarResultado");
 const funcionarioDao = new FuncionarioDao();
 const usuarioController = new UsuarioController();
 
 class FuncionarioController {
+  consultaPorEmail(req, res){
+    const email = req.params.email;
+
+    funcionarioDao.listarPorEmail(email, (erro, resultado)=>{
+      if(erro){
+        console.log('Ocorreu um erro ' + erro);
+        res.status(500).send('Ocorreu um erro!');
+        return;
+      }
+      verificarResultado(resultado, res);
+    });
+  }
+  consultaPorNome(req, res){
+    const nome = req.params.nome;
+
+    funcionarioDao.listarPorNome(nome, (erro, resultado)=>{
+      if(erro){
+        console.log('Ocorreu um erro ' + erro);
+        res.status(500).send('Ocorreu um erro!');
+        return;
+      }
+      verificarResultado(resultado, res);
+    });
+  }
+  consultaPorCpf(req, res){
+    const cpf = req.params.cpf;
+
+    funcionarioDao.listarPorCpf(cpf, (erro, resultado)=>{
+      if(erro){
+        console.log('Ocorreu um erro ' + erro);
+        res.status(500).send('Ocorreu um erro!');
+        return;
+      }
+      verificarResultado(resultado, res);
+    });
+  }
+  consultaPorCargo(req, res){
+    const cargo = req.params.cargo;
+
+    funcionarioDao(cargo, (erro, resultado)=>{
+      if(erro){
+        console.log('Ocorreu um erro ' + erro);
+        res.status(500).send('Ocorreu um erro!');
+        return;
+      }
+      verificarResultado(resultado, res);
+    });
+  }
+  
   cadastro(req, res) {
     const funcionario = req.body;
 
--- Backend/dao/FuncionarioDao.js
+++ Backend/dao/FuncionarioDao.js
@@ -1,6 +1,38 @@
 const conexao = require("../config/conexao");
 
 class FuncionarioDao {
+  listarPorEmail(email, callback){
+    const sql = `
+      SELECT nome, rg, cpf, cargo, end, bairro, cidade, cep, fone, email
+      FROM funcionarios
+      WHERE email = '${email}'
+    `
+    conexao.query(sql, callback);
+  }
+  listarPorNome(nome, callback){
+    const sql = `
+      SELECT nome, rg, cpf, cargo, end, bairro, cidade, cep, fone, email
+      FROM funcionarios
+      WHERE lcase(nome LIKE '%${nome}%')
+    `
+    conexao.query(sql, callback);
+  }
+  listarPorCpf(cpf, callback){
+    const sql = `
+      SELECT nome, rg, cpf, cargo, end, bairro, cidade, cep, fone, email
+      FROM funcionarios
+      WHERE cpf = '${cpf}'
+    `
+    conexao.query(sql, callback);
+  }
+  listarPorNome(cargo, callback){
+    const sql = `
+      SELECT nome, rg, cpf, cargo, end, bairro, cidade, cep, fone, email
+      FROM funcionarios
+      WHERE lcase(cargo LIKE '%${cargo}%')
+    `
+    conexao.query(sql, callback);
+  }
   inserir(funcionario, callback) {
     const sql = `
             INSERT INTO funcionarios(nome, rg, cpf, cargo, end, bairro, cidade, cep, fone, email, senha)
